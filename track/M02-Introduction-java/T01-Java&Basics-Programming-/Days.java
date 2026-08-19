e0e07fb656492c86438dabd8244f1564b4b87206
aravindkumar <aravindkumarcs216@gmail.com>
2026-08-19 22:42:25 +0530

    statement using switch control

---
+++ track/M02-Introduction-java/T01-Java&Basics-Programming-/Days.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+public class Days {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Number :");
+        int days = sc.nextInt();
+        switch (days) {
+            case 123:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            default:
+                System.out.println("Error Invalid Day");
+        }
+
+    }
+}
