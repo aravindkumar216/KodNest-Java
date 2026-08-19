e6eccba6b33f562d7bd0d0e711dd4070e99319a1
aravindkumar <aravindkumarcs216@gmail.com>
2026-08-19 22:49:10 +0530

    welcome to java programming

---
+++ track/M01-understanding-java/src/WelcomeToJava.java
@@ -0,0 +1,9 @@
+
+public class WelcomeToJava {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to java Track");
+        System.out.println("I already know how to built programming logic.");
+        System.out.println("Now I am learning how to express the logic using java.");
+    }
+}
