30d94853e312e4a7427b2380d90ac3faae62a4e5
aravindkumar <aravindkumarcs216@gmail.com>
2026-08-20 15:54:41 +0530

    nested

---
+++ track/M02-Introduction-java/T02-Control flow and looping/nested.java
@@ -0,0 +1,24 @@
+
+public class nested {
+
+    public static void main(String[] args) {
+
+        boolean kfc = true;
+        boolean wings = true;
+        boolean overPriced = true;
+
+        if (kfc) {
+            System.out.println("entering into kfc");
+            if (wings) {
+                System.out.println("checking for wings");
+                if (overPriced) {
+                    System.out.println("but its costly so i will buy chicken");
+                } else {
+                    System.out.println("i will buy wings");
+                }
+            }
+        } else {
+            System.out.println("going to other shop");
+        }
+    }
+}
