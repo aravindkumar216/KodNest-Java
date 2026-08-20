1f7aaa1ad72efafca210601bef57c962662fdb4b
aravindkumar <aravindkumarcs216@gmail.com>
2026-08-20 13:19:55 +0530

    else if ladder

--- track/M02-Introduction-java/T02-Control flow and looping/Condition.java
+++ track/M02-Introduction-java/T02-Control flow and looping/Condition.java
@@ -4,16 +4,23 @@ import java.util.Scanner;
 public class Condition {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter roll no");
-        int rollno1 = scanner.nextInt();
-        int rollno2 = scanner.nextInt();
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the marks (0 - 100): ");
+        int num = scan.nextInt();
+        /* >=90 grade A
+        >=80 grade B
+	       >=35 grade C
+         */
 
-        if (rollno1 > rollno2) {
-            System.out.println("roll no 1 is greater");
+        if (num >= 90) {
+            System.out.println("GRADE A");
+        } else if (num >= 80) {
+            System.out.println("GRADE B");
+        } else if (num >= 35) {
+            System.out.println("GRADE C");
         } else {
-            System.out.println("roll no 2 greater");
+            System.out.println("Study well bro");
         }
-        scanner.close();
+        scan.close();
     }
 }
